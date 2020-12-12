077d3d03ee1ae0611ff78c59de10a5064f98f727
MaxHwoy <max.hwoy@gmail.com>
2020-12-11 20:59:28 -0800

    Version bump with updated CoreExtensions lib linked.

--- Binary/Editor.cs
+++ Binary/Editor.cs
@@ -872,7 +872,7 @@ namespace Binary
 
 		private void EMSHelpAbout_Click(object sender, EventArgs e)
 		{
-			MessageBox.Show("Binary by MaxHwoy v2.4.0", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			MessageBox.Show("Binary by MaxHwoy v2.5.0", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
 		}
 		
 		private void EMSHelpTutorials_Click(object sender, EventArgs e)
